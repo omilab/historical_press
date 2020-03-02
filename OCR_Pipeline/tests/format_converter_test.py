be49ea9e84edf9154fff3226bee62c4aac86a07d
yanirmr <yanirmr@gmail.com>
2020-03-02 21:51:20 +0200

    Update format_converter_test.py
    tests for the new functionalities (relative, absolute paths)
    the tests now are independent and could run in any computer with the attached files and folders

--- OCR_Pipeline/tests/format_converter_test.py
+++ OCR_Pipeline/tests/format_converter_test.py
@@ -1,5 +1,4 @@
-#  For these test you need the paths like they appear below or change them for your directory. 
-
+# For these test you need the files in the folder "resources_for_tests"
 
 import unittest
 from unittest.mock import patch
@@ -14,29 +13,56 @@ class MyTestCase(unittest.TestCase):
 
 
 class InputFromUser(unittest.TestCase):
-    def test_get_path_from_user(self):
+    def test_get_empty_path_from_user(self):
         with patch('builtins.input', return_value=""):
+            self.assertEqual(fc.get_path_from_user(), os.path.dirname(__file__))
+
+        with patch('builtins.input', return_value="    "):
+            self.assertEqual(fc.get_path_from_user(), os.path.dirname(__file__))
+
+
+    def test_get_illegal_path_from_user(self):
+        with patch('builtins.input', return_value="123456789"):
             self.assertEqual(fc.get_path_from_user(), "")
 
-        with patch('builtins.input', return_value="no"):
-            self.assertEqual(fc.get_path_from_user(), "")
-
+    def test_get_absolute_path_from_user(self):
+        work_dir = os.path.dirname(__file__)
+        abs_path = os.path.join(work_dir, "resources_for_tests")
         with patch('builtins.input',
-                   return_value="C:\\Users\\User\\Documents\\Projects\\OmiLab\\new pipeline\\data\\1914-11-06-TEST\\output"):
+                   return_value=abs_path):
+            self.assertEqual(fc.get_path_from_user(), abs_path)
+
+    def test_get_relative_path_from_user(self):
+        with patch('builtins.input',
+                   return_value="resources_for_tests"):
+            work_dir = os.path.dirname(__file__)
             self.assertEqual(fc.get_path_from_user(),
-                             "C:\\Users\\User\\Documents\\Projects\\OmiLab\\new pipeline\\data\\1914-11-06-TEST\\output")
+                             os.path.join(work_dir, "resources_for_tests"))
 
 
 class PathTraverse(unittest.TestCase):
-    def test_find_sub_folders_with_toc_file(self):
+    def test_find_sub_folders_with_toc_file_empty_case(self):
         # path1 and path3 contain toc.xmk file into them
-        path0 = "C:\\Users\\User\\Documents\\Projects\\OmiLab\\new pipeline\\data\\1914-11-06-TEST\\output"
+        work_dir = os.path.dirname(__file__)
+        relative_path0 = "resources_for_tests\\1920-01-06\\Document\\1\\Img"
+        path0 = os.path.join(work_dir, relative_path0)
         self.assertEqual(fc.find_sub_folders_with_toc_file(path0), [])
-        path1 = "C:\\Users\\User\\Documents\\Projects\\OmiLab\\new pipeline\\data\\1914-11-06-TEST"
+
+    def test_find_sub_folders_with_toc_file_one_folder_case(self):
+        work_dir = os.path.dirname(__file__)
+        relative_path1 = "resources_for_tests\\1920-01-06"
+        path1 = os.path.join(work_dir, relative_path1)
         self.assertEqual(fc.find_sub_folders_with_toc_file(path1), [path1])
-        path2 = "C:\\Users\\User\\Documents\\Projects\\OmiLab\\new pipeline\\data"
-        path3 = "C:\\Users\\User\\Documents\\Projects\\OmiLab\\new pipeline\\data\\1920-01-06"
-        self.assertEqual(set(fc.find_sub_folders_with_toc_file(path2)), {path1, path3})
+
+    def test_find_sub_folders_with_toc_file_multi_folder_case(self):
+        work_dir = os.path.dirname(__file__)
+        relative_parent_path = "resources_for_tests"
+        parent_path = os.path.join(work_dir, relative_parent_path)
+        relative_sub_folder1_path = "resources_for_tests\\1920-01-06"
+        sub_folder1_path = os.path.join(work_dir, relative_sub_folder1_path)
+        relative_sub_folder2_path = "resources_for_tests\\1914-11-06"
+        sub_folder2_path = os.path.join(work_dir, relative_sub_folder2_path)
+        self.assertEqual(set(fc.find_sub_folders_with_toc_file(parent_path)), {sub_folder1_path, sub_folder2_path})
 
 
 class CreateOutputDirectory(unittest.TestCase):
@@ -48,7 +74,9 @@ class CreateOutputDirectory(unittest.TestCase):
         return folders
 
     def test_create_unique_output_folder(self):
-        src_path = "C:\\Users\\User\\Documents\\Projects\\OmiLab\\new pipeline\\data"
+        work_dir = os.path.dirname(__file__)
+        relative_path = "resources_for_tests"
+        src_path = os.path.join(work_dir, relative_path)
         before_adding_output_folder = self.subfolders_counter(src_path)
         output_folder = fc.create_unique_output_folder(src_path)
         after_adding_output_folder = self.subfolders_counter(src_path)
